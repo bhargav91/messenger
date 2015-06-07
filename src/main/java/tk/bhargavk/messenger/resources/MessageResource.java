ea2a17a292f5462f00b79de595200b59cc7e5cb1
Bhargav Kaneria <bhargav.kaneriya@gmail.com>
2015-06-07 16:02:07 +0530

    Implemented a POST method
    Implemented add message method that lets you add a message

--- src/main/java/tk/bhargavk/messenger/resources/MessageResource.java
+++ src/main/java/tk/bhargavk/messenger/resources/MessageResource.java
@@ -2,7 +2,9 @@ package tk.bhargavk.messenger.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -21,12 +23,19 @@ public class MessageResource {
 	public List<Message> getMessages(){
 		return messageService.getAllMessages();
 	}
-	
-	
+		
 	@GET
 	@Path("/{messageId}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Message getMessage(@PathParam("messageId") long messageId){
 		return messageService.getMessage(messageId);
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message addMessage(Message msg){
+		return messageService.addMessage(msg);
+		
 	}
 }
